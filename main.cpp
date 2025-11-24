f18dfea8cc7e6ab05f36fd3f17f0414970bf7e42
root <root@Asus.localdomain>
2025-11-24 12:57:53 +0300

    CW3: commit 3

--- main.cpp
+++ main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
 
-struct Planer
+struct Planar
 {
   virtual int x() const = 0;
   virtual int y()const = 0;
   virtual int abc_sqt() const = 0;
-  virtual ~Planer() = default;
+  virtual ~Planar() = default;
 };
 
-Planer * make(std::istream & is);
-Planer * most_left (Planer ** pls, size_t k);
+Planar * make(std::istream & is);
+Planar * make(size_t pl);
+void draw(Planar * pl);
+void free_planars(Planar ** pls, size_t k);
+Planar * most_left (Planar ** pls, size_t k);
 
 
 int mai()
 {
-  Planer * pls[10] = {};
+  Planar * pls[10] = {};
   size_t k = 0;
+  for(size_t i=0; i < 10; ++i){
+    try
+    {
+      pls[k]= make(i%2);
+    } catch(...){
+      free_planars(pls, k);
+      return 2;
+    };
+    k++;
+};
+draw(most_left(pls ,k));
+free_planars(pls, k);
   // pls[k++] = make(std::cin);
-  Planer * left = most_left(pls,k);
+  Planar * left = most_left(pls,k);
   //draw(left);
   //free_planars(pls,k); 
 }
