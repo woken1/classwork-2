2e58b5ba42b66189d5dcb91006597ebd22acafda
root <root@Asus.localdomain>
2025-11-24 12:48:57 +0300

    CW2: commit num_2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
-int main(){
-    return 0;
+struct Planer
+{
+  virtual int x() const = 0;
+  virtual int y()const = 0;
+  virtual int abc_sqt() const = 0;
+  virtual ~Planer() = default;
+};
+
+Planer * make(std::istream & is);
+Planer * most_left (Planer ** pls, size_t k);
+
+
+int mai()
+{
+  Planer * pls[10] = {};
+  size_t k = 0;
+  // pls[k++] = make(std::cin);
+  Planer * left = most_left(pls,k);
+  //draw(left);
+  //free_planars(pls,k); 
 }
