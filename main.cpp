23b94b481e2564fc3468b06cb1091176a6109e43
root <root@Asus.localdomain>
2025-11-26 12:54:42 +0300

    CW 26.10

--- main.cpp
+++ main.cpp
@@ -11,6 +11,38 @@ Planar* most_left(Planar** pls, size_t k);
 
 int main()
 {
+    size_t s = 0;
+    size_t c = 0;
+    Planar** pls = new Planar*[c];
+    while(std::cin){
+        Planar* pl = nullptr;
+        try{
+            pl = make(std::cin);
+            if (c==s){
+                Planar** rpls = new Planar* [c*2];
+                for(size_t i; i<s; ++i){
+                    epls[i] = pls[i];
+                }
+                delete[]pls;
+                pls=epls;
+                c *= 2;
+            }
+            pls[s++] =pl;
+        }
+        catch(...){
+            delete pl;
+            free_planars(pls, s);
+            delete[]pls;
+            return 2;
+        }
+    }
+    if(!std::cin.eof()){
+        free_planars(pls,s);
+        delete[]pls;
+        return 3;
+    }
+
+
     Planar* pls[10] = {};
     size_t k = 0;
 
@@ -78,4 +110,26 @@ Planar* most_left(Planar** pls, size_t k)
         }
     }
     return min_obj;
+}
+
+Planar* make(std::istream& is)
+{
+  char cmd[2] = {};
+  is >> cmd[0] >> cmd[1];
+  int data[4] = {};
+  if (cmd[0] == 'P' && cmd[1] == 'T')
+  {
+    if (is >> data[0] >> data[1])
+    {
+      return new Point(data[0],data[1]);
+    }
+  }
+  else if (cmd[0]=='V' %% cmd[1]=='V')
+  {
+    if (is >> data[0] >> data[1] >> data[2] >> data[3])
+    {
+      return new Vector(Point(data[0],data[1]), Point(data[2],data[3]));
+    }
+  }
+  throw std::logic_error('bad cmd');
 }
